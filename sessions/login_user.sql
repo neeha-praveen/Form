b2838c0400cb9332f027cd8f01a9261cfc9444f3
neeha-praveen <neeha.npraveen@gmail.com>
2025-01-09 12:10:05 +0530

    add session files

---
+++ sessions/login_user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `login_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
