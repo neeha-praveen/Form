ad6b5a58d1d4524d761e5d78f2c0d54284ded861
neeha-praveen <neeha.npraveen@gmail.com>
2025-01-15 09:14:31 +0530

    updated files

---
+++ webtech-sessions/config.php
@@ -0,0 +1,8 @@
+<?php 
+  //MySQL Db Connection
+  $host_name="localhost";
+  $user_name="root";
+  $password="";
+  $db_name="mydatabase";
+  $con=mysqli_connect($host_name, $user_name, $password, $db_name);
+?>
